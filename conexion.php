ad406c08c451d2f67eda47efb327b5052f980d97
javi125-marker <jr365502@gmail.com>
2022-06-06 16:20:21 -0500

    Agregando nueva conexión

--- conexion.php
+++ conexion.php
@@ -1,15 +1,16 @@
 <?php
-//DATOS DE CONEXIÓN HACIA LA BDD
-define('SERVERNAME', 'localhost');
-define('USERNAME', 'root');
-define('PASSWORD', '');
-define('DBNAME', 'cliente');
 
-//creación de la conexión a la base de datos con mysql
-$conn = new mysqli(SERVERNAME, USERNAME, PASSWORD, DBNAME);
+    //DATOS DE CONEXIÓN HACIA LA BDD
+    define('SERVERNAME', 'localhost');
+    define('USERNAME', 'root');
+    define('PASSWORD', '');
+    define('DBNAME', 'cliente');
 
-//Controlar la conexión
-if($conn -> connect_error){
+    //creación de la conexión a la base de datos con mysql
+    $conn = new mysqli(SERVERNAME, USERNAME, PASSWORD, DBNAME);
+
+    //Controlar la conexión
+    if($conn -> connect_error){
     die('Conexión fallida:' . $conn -> connect_error);
-}
+    }
 ?>
